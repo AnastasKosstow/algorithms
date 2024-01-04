7dafa87da52f39998eedf6b9558e40867c839397
Anastas Kostov <anastas.kostov@tbs.tech>
2024-01-04 12:02:57 +0200

    linked_list impl

--- rust/src/collections/linked_list.rs
+++ rust/src/collections/linked_list.rs
@@ -1,10 +1,11 @@
 #![allow(dead_code)]
 use std::ptr::NonNull;
 
-#[derive(Clone, Copy)]
+#[derive(Eq, PartialEq, Clone, Copy)]
 pub struct Node<N> {
     pub value: N,
-    pub next: Option<NonNull<Node<N>>>
+    pub next: Option<NonNull<Node<N>>>,
+    prev: Option<NonNull<Node<N>>>
 }
 
 pub struct LinkedList<N> {
@@ -13,7 +14,7 @@ pub struct LinkedList<N> {
     pub length: usize,
 }
 
-impl<N: Clone> LinkedList<N> {
+impl<N: Clone + Eq + PartialEq> LinkedList<N> {
     pub fn new() -> Self {
         LinkedList { 
             head: None, 
@@ -23,13 +24,14 @@ impl<N: Clone> LinkedList<N> {
     }
 
     pub fn is_empty(&self) -> bool {
-        self.length > 0
+        self.length == 0
     }
 
     pub fn add(&mut self, value: N) {
         let node: Node<N> = Node { 
             value, 
-            next: None 
+            next: None,
+            prev: self.tail
         };
 
         unsafe {
@@ -80,5 +82,64 @@ impl<N: Clone> LinkedList<N> {
             }
         }
         Some(vec)
+    }
+
+    pub fn delete(&mut self, value: N) {
+        if self.is_empty() {
+            return;
+        }
+
+        let node: Option<NonNull<Node<N>>> = self.head;
+        unsafe {
+            self.delete_node(value, node);
+        }
+    }
+
+    unsafe fn delete_node(&mut self, value: N, mut node: Option<NonNull<Node<N>>>) {
+        while let Some(current_node) = node {
+            if current_node.as_ref().value == value {
+                let old_ptr = Box::from_raw(current_node.as_ptr());
+
+                if let Some(mut prev) = old_ptr.prev {
+                    prev.as_mut().next = old_ptr.next;
+                }
+
+                if let Some(mut next) = old_ptr.next {
+                    next.as_mut().prev = old_ptr.prev;
+                }
+
+                self.length -= 1;
+                break;
+            }
+            node = (*current_node.as_ptr()).next;
+        }
+    }
+
+    pub fn clear(&mut self) {
+        unsafe {
+            self.clear_all_nodes();
+        }
+        
+        self.head = None;
+        self.tail = None;
+        self.length = 0;
+    }
+
+    unsafe fn clear_all_nodes(&mut self) {
+        while let Some(node) = self.head {
+            let boxed_node = Box::from_raw(node.as_ptr());
+            self.head = boxed_node.next;
+        }
+    }
+}
+
+impl<N> Drop for LinkedList<N> {
+    fn drop(&mut self) {
+        while let Some(node) = self.head {
+            unsafe {
+                let boxed_node = Box::from_raw(node.as_ptr());
+                self.head = boxed_node.next;
+            }
+        }
     }
 }
