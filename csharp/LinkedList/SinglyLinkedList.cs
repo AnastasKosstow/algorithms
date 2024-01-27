6e85141a471640e13123e3d3a8e4e7093e866885
Anastas Kostov <anastas.kostov@tbs.tech>
2024-01-27 23:43:43 +0200

    linked-list csharp impl

---
+++ csharp/LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,111 @@
+﻿namespace LinkedList;
+
+public class SinglyLinkedList<T>
+{
+    private class Node
+    {
+        public T Data;
+        public Node Next;
+
+        public Node(T data)
+        {
+            this.Data = data;
+            this.Next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public int Length
+    {
+        get { return this.length; }
+        private set { length = value; }
+    }
+
+    public SinglyLinkedList()
+    {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+
+    public bool IsEmpty()
+    {
+        return length == 0;
+    }
+
+    public void Add(T data)
+    {
+        var node = new Node(data);
+
+        if (IsEmpty())
+        {
+            head = tail = node;
+        }
+        else
+        {
+            tail.Next = node;
+            tail = node;
+        }
+
+        length++;
+    }
+
+    public T Get(int index)
+    {
+        if (IsEmpty() || index < 0 || index >= length)
+        {
+            throw new IndexOutOfRangeException("Index out of range.");
+        }
+
+        var current = head;
+        for (int i = 0; i < index; i++)
+        {
+            current = current.Next;
+        }
+
+        return current.Data;
+    }
+
+    public void Delete(int index)
+    {
+        if (IsEmpty() || index < 0 || index >= length)
+        {
+            throw new IndexOutOfRangeException("Index out of range.");
+        }
+
+        if (index == 0)
+        {
+            head = head.Next;
+            if (length == 1)
+            {
+                tail = null;
+            }
+        }
+        else
+        {
+            var current = head;
+            for (int i = 0; i < index - 1; i++)
+            {
+                current = current.Next;
+            }
+
+            current.Next = current.Next.Next;
+            if (index == length - 1) // If the last element is being deleted
+            {
+                tail = current;
+            }
+        }
+
+        length--;
+    }
+
+    public void Clear()
+    {
+        head = null;
+        tail = null;
+        length = 0;
+    }
+}
---
+++ csharp/RadixSort/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] arr = new[] { 7253, 3864, 573, 8511, 9619, 2608, 5455, 571, 1, 0 };
+arr = Sort(arr);
+
+int[] Sort(int[] sourceArray)
+{
+    throw new NotImplementedException();
+}
