f6b1178a655e6df3cb1dc1e87167dfefbcbcbbed
Anastas Kostov <anastas.kostov@tbs.tech>
2024-01-29 15:19:50 +0200

    union-find

--- rust/src/data_structures/disjoint_set.rs
+++ rust/src/data_structures/disjoint_set.rs
@@ -1,10 +1,14 @@
 pub struct DisjointSet {
-    parent: Vec<usize>
+    parent: Vec<usize>,
+    rank: Vec<usize>
 }
 
 impl DisjointSet {
     pub fn new(size: usize) -> Self {
-        DisjointSet { parent: (0..size).collect() }
+        DisjointSet { 
+            parent: (0..size).collect(),
+            rank: vec![0; size]
+        }
     }
 
     pub fn find(&mut self, index: usize) -> usize {
@@ -17,8 +21,20 @@ impl DisjointSet {
     pub fn union(&mut self, x: usize, y: usize) {
         let x_root = self.find(x);
         let y_root = self.find(y);
-        if x_root != y_root {
+
+        if x_root == y_root {
+            return;
+        }
+
+        if self.rank[x_root] < self.rank[y_root] {
             self.parent[x_root] = y_root;
+        }
+        else if self.rank[x_root] > self.rank[y_root] {
+            self.parent[y_root] = x_root;
+        }
+        else {
+            self.parent[y_root] = x_root;
+            self.rank[x_root] += 1;
         }
     }
 }
